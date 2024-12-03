2425331de443da54ddb280c952f019a6930d6b3e
T P <t.m.phuong@hotmail.com>
2024-12-03 22:06:20 +0100

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from "./components/card/Card.jsx";
+
+function App() {
+
+  return (
+    <>
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,9 @@
+import "./Card.css"
+
+const Card = () => {
+    return (
+        <div className={"card"}>I&#39;m a card with an inner glow</div>
+    )
+}
+
+export default Card
